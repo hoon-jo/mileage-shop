cc8b20d4077517673a657b27017e246cd3f4217f
hoon-jo <hoon.jo@zipida.com>
2020-12-22 23:40:28 +0900

    Refactor and Implement starting mileage shop program

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	deli "github.com/hoon-jo/mileage-shop/pkg/delivery"
+	item "github.com/hoon-jo/mileage-shop/pkg/product"
+	user "github.com/hoon-jo/mileage-shop/pkg/user"
 )
 
 func main() {
+	deliveryCount := 0
+	buyer := user.CreateBuyer()
+	products := item.CreateProducts()
+	deliverystart := make(chan bool)
+	deliveryList := deli.NewDelivery(deliverystart, deliveryCount)
 
 	for {
 		menu := 0 // 첫 메뉴
@@ -20,33 +29,29 @@ func main() {
 		fmt.Scanln(&menu)
 		fmt.Println()
 
-		if menu == 1 { // 물건 구매
+		switch menu {
 
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
-			fmt.Scanln()
-		} else if menu == 2 { // 남은 수량 확인
-
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
-			fmt.Scanln()
-		} else if menu == 3 { // 잔여 마일리지 확인
-
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
-			fmt.Scanln()
-		} else if menu == 4 { // 배송 상태 확인
-
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
-			fmt.Scanln()
-		} else if menu == 5 { // 장바구니 확인
-
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
-			fmt.Scanln()
-		} else if menu == 6 { // 프로그램 종료
+		case 1:
+			user.BuyProduct(products, buyer, &deliveryCount, deliverystart)
+		case 2:
+			item.GetProducts(products)
+		case 3:
+			user.GetMaileage(buyer)
+		case 4:
+			deli.GetDeliveryStatus(deliveryList)
+		case 5:
+			user.ViewMyCart(products, buyer)
+		case 6:
 			fmt.Println("프로그램을 종료합니다.")
-
-			return // main함수 종료
-		} else {
-			fmt.Println("잘못된 입력입니다. 다시 입력해주세요.\n")
+			return
+		default:
+			fmt.Println("잘못된 입력입니다. 다시 입력해주세요.")
 		}
+		if menu > 0 && menu < 7 {
+			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Scanln()
+		}
+
 	}
 
 }
